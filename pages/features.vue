19bc641d532822f73e3d15daa6fc00ff81f10a67
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2025-06-23 19:43:36 +0300

    test

--- pages/features.vue
+++ pages/features.vue
@@ -3,36 +3,33 @@
     <v-carousel :height="carouselHeight" show-arrows="false" cycle hide-delimiter-background>
       <v-carousel-item>
         <div class="relative">
-          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://assets.e3arabi.com/wp-content/uploads/2021/01/online-3412473_640-3.jpg" alt="Image" @click="navigateTo('/article')" />
+          <img class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://assets.e3arabi.com/wp-content/uploads/2021/01/online-3412473_640-3.jpg" alt="Image" />
          
         </div>
       </v-carousel-item>
       <v-carousel-item>
         <div class="relative">
-          <img v-if="$i18n.locale === 'ar-AR'" class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk8gQy8sBOFgRHsDO4uUUrnr_EGVswXPK_y13N9690vI6B1xddHCLNELJBQzrhAF3zPiA&usqp=CAU" alt="Image"  />
-          <img v-else class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk8gQy8sBOFgRHsDO4uUUrnr_EGVswXPK_y13N9690vI6B1xddHCLNELJBQzrhAF3zPiA&usqp=CAU" alt="Image"  />
+          <img v-if="$i18n.locale === 'ar-AR'" class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://iqdamacademy.com/media/main2.jpeg" alt="Image"  />
+          <img v-else class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://iqdamacademy.com/media/main2.jpeg" alt="Image"  />
+         
+        </div>
+      </v-carousel-item>
+      <v-carousel-item>
+        <div class="relative">
+          <img v-if="$i18n.locale === 'ar-AR'" class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://i.ytimg.com/vi/XJfXBlhT5H8/hqdefault.jpg" alt="Image"  />
+          <img v-else class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://i.ytimg.com/vi/XJfXBlhT5H8/hqdefault.jpg" alt="Image"  />
+         
+        </div>
+      </v-carousel-item>
+      <v-carousel-item>
+        <div class="relative">
+          <img v-if="$i18n.locale === 'ar-AR'" class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://scontent.fcai19-8.fna.fbcdn.net/v/t39.30808-6/490358802_664285513427644_7818927235157923863_n.png?stp=dst-png_s960x960&_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEJiSsIVEPMMA0EViV0MifBTO1f5yt88SVM7V_nK3zxJYdfYgOjvVyk_LBUMbU0iVebEIH--Vd2D6YTlLDp22I2&_nc_ohc=0G-9a58rLosQ7kNvwH7_opi&_nc_oc=Adl_rfJGbEn1Qg4huEBf-qc6ch0Gs7q133ZRUd4PymoxKleBpJUjZ4aHk4C5R-JEKjo&_nc_zt=23&_nc_ht=scontent.fcai19-8.fna&_nc_gid=N_tEgGojKfQkmxOpu-I4DQ&oh=00_AfNU-vIABtdpRJ-Jbu-Z6uUe-8Sf47C1__wes094oDaC1Q&oe=685F66F8" alt="Image"  />
+          <img v-else class="sm:w-100 md:w-[70%] px-4 rounded-xl relative cursor-pointer md:h-[400px] mx-auto" src="https://scontent.fcai19-8.fna.fbcdn.net/v/t39.30808-6/490358802_664285513427644_7818927235157923863_n.png?stp=dst-png_s960x960&_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEJiSsIVEPMMA0EViV0MifBTO1f5yt88SVM7V_nK3zxJYdfYgOjvVyk_LBUMbU0iVebEIH--Vd2D6YTlLDp22I2&_nc_ohc=0G-9a58rLosQ7kNvwH7_opi&_nc_oc=Adl_rfJGbEn1Qg4huEBf-qc6ch0Gs7q133ZRUd4PymoxKleBpJUjZ4aHk4C5R-JEKjo&_nc_zt=23&_nc_ht=scontent.fcai19-8.fna&_nc_gid=N_tEgGojKfQkmxOpu-I4DQ&oh=00_AfNU-vIABtdpRJ-Jbu-Z6uUe-8Sf47C1__wes094oDaC1Q&oe=685F66F8" alt="Image"  />
          
         </div>
       </v-carousel-item>
      
-      <v-carousel-item v-for="(product, index) in products" :key="index">
-        <div class="relative">
-          <img class="sm:w-100 px-4 rounded-xl md:h-[400px] mx-auto md:w-[70%] "
-               :src="product.imageUrl || '/public/imgs/building.jpg'" alt="Product Image"  />
-               <h1 class="absolute  bg-violet-700 hover:bg-violet-900 delayed text-white text-sm rounded-full px-1 bottom-0  md:hidden cursor-pointer"
-            :class="{ 'left-12 ': $i18n.locale === 'ar-AR', 'right-12 ': $i18n.locale === 'en-US' }" @click="navigateTo(`/product/${product.id}`)">
-            {{ $t('More') }}
-        </h1>
-        <h1 class="absolute  bg-violet-700 hover:bg-violet-900 delayed text-sm text-white rounded-full px-4 py-1 bottom-2 hidden md:block lg:hidden cursor-pointer"
-            :class="{ 'left-64 ': $i18n.locale === 'ar-AR', 'right-64 ': $i18n.locale === 'en-US' }" @click="navigateTo(`/product/${product.id}`)">
-            {{ $t('More') }}
-        </h1>
-        <h1 class="absolute  bg-violet-700 hover:bg-violet-900 delayed text-sm text-white rounded-full px-4 py-1 bottom-2 hidden lg:block cursor-pointer"
-            :class="{ 'left-80 ': $i18n.locale === 'ar-AR', 'right-80 ': $i18n.locale === 'en-US' }" @click="navigateTo(`/product/${product.id}`)">
-            {{ $t('More') }}
-        </h1>
-        </div>
-      </v-carousel-item>
+     
      
     </v-carousel>
   </div>
