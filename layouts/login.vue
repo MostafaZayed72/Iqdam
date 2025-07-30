40d1ac9e6b51842dcbf2d5fd1549e4d160d6e5c3
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2025-07-30 17:32:46 +0300

    y

--- layouts/login.vue
+++ layouts/login.vue
@@ -5,8 +5,7 @@
       <div class="flex items-center gap-4">
         <!-- <Icon @click="toggleSidebar" name="iconamoon:menu-burger-horizontal"
             class="md:hidden text-xl text-white cursor-pointer" /> -->
-        <NuxtImg src="/imgs/logo.png" class="w-12 h-12 cursor-pointer bg-white" alt="" style="border-radius: 50%;"
-          @click="navigateTo('/')" />
+        <NuxtImg src="/imgs/logo.png" class="w-12 h-12 cursor-pointer bg-white" alt="" style="border-radius: 50%;" @click="navigateTo('/')" />
       </div>
 
       <!-- الروابط في الشاشات الكبيرة -->
